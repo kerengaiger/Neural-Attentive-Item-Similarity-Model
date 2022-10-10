f8db266b43e971eae1418f638192e28f8bc9e613
kerengaiger <keren.gaiger@gmail.com>
2022-10-10 11:44:01 +0300

    Dataset.py: remove rating read

--- Dataset.py
+++ Dataset.py
@@ -75,9 +75,8 @@ class Dataset(object):
             line = f.readline()
             while line != None and line != "":
                 arr = line.split("\t")
-                user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
-                if (rating > 0):
-                    mat[user, item] = 1.0
+                user, item = int(arr[0]), int(arr[1])
+                mat[user, item] = 1.0
                 line = f.readline()
         print "already load the trainMatrix..."
         return mat
